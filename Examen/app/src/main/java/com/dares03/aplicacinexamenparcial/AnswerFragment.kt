e3dc698631e8afb72d6bfc58b1f5164fe138c422
josshuauls <jfloresch@ulasalle.edu.pe>
2024-05-11 21:19:10 -0500

    V 0.7 Validacion de la respuesta

--- Examen/app/src/main/java/com/dares03/aplicacinexamenparcial/AnswerFragment.kt
+++ Examen/app/src/main/java/com/dares03/aplicacinexamenparcial/AnswerFragment.kt
@@ -3,9 +3,22 @@ package com.dares03.aplicacinexamenparcial
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.widget.TextView
+import kotlin.properties.Delegates
 
 class AnswerFragment : Fragment(R.layout.fragment_answer) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        var respuesta by Delegates.notNull<Boolean>()
+        arguments?.let {
+            respuesta = it.getBoolean("rappy")
+        }
+        val txtRespuesta = view.findViewById<TextView>(R.id.valor_respuesta)
+        if(respuesta) {
+            txtRespuesta.text = "Correcto"
+        }
+        else{
+            txtRespuesta.text = "Incorrecto"
+        }
     }
 }
