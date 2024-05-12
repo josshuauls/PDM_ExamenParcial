abe461fb59de2da8f0766d5c865c661fcf405574
josshuauls <jfloresch@ulasalle.edu.pe>
2024-05-11 22:06:30 -0500

    V 1.0 Juego Funcional (Se puede mejorar)

--- Examen/app/src/main/java/com/dares03/aplicacinexamenparcial/AnswerFragment.kt
+++ Examen/app/src/main/java/com/dares03/aplicacinexamenparcial/AnswerFragment.kt
@@ -3,12 +3,27 @@ package com.dares03.aplicacinexamenparcial
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import kotlin.properties.Delegates
 
 class AnswerFragment : Fragment(R.layout.fragment_answer) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val respuesta_detallada = view.findViewById<TextView>(R.id.respuesta_detallada)
+        when(numPregunta){
+            1 -> {
+                respuesta_detallada.text = getString(R.string.descripcion_1)
+            }
+            2 -> {
+                respuesta_detallada.text = getString(R.string.descripcion_2)
+            }
+            3 -> {
+                respuesta_detallada.text = getString(R.string.descripcion_3)
+            }
+        }
         var respuesta by Delegates.notNull<Boolean>()
         arguments?.let {
             respuesta = it.getBoolean("rappy")
@@ -19,6 +34,20 @@ class AnswerFragment : Fragment(R.layout.fragment_answer) {
         }
         else{
             txtRespuesta.text = "Incorrecto"
+        }
+
+        val continuar = view.findViewById<Button>(R.id.button_continue)
+        continuar.setOnClickListener(){
+            numPregunta++
+            if(numPregunta <= 3) {
+                findNavController().navigate(R.id.questionFragment)
+            }
+            else{
+                numPregunta = 1
+                Toast.makeText(context,"Tienes $puntuacion respuestas correctas",Toast.LENGTH_LONG).show()
+                puntuacion = 0
+                findNavController().popBackStack(R.id.welcomeFragment, false)
+            }
         }
     }
 }
--- Examen/app/src/main/java/com/dares03/aplicacinexamenparcial/QuestionFragment.kt
+++ Examen/app/src/main/java/com/dares03/aplicacinexamenparcial/QuestionFragment.kt
@@ -4,19 +4,49 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 
 var numPregunta = 1
+var puntuacion = 0
 class QuestionFragment : Fragment(R.layout.fragment_question) {
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val chamo = Bundle()
+        val pregunta = view.findViewById<TextView>(R.id.questionText)
         val option1 = view.findViewById<Button>(R.id.option1)
         val option2 = view.findViewById<Button>(R.id.option2)
         val option3 = view.findViewById<Button>(R.id.option3)
         val option4 = view.findViewById<Button>(R.id.option4)
+        fun preguntaActual(){
+            when(numPregunta){
+                1 ->{
+                    pregunta.text = getString(R.string.pregunta_1)
+                    option1.text = getString(R.string.respuesta_1_1)
+                    option2.text = getString(R.string.respuesta_1_2)
+                    option3.text = getString(R.string.respuesta_1_3)
+                    option4.text = getString(R.string.respuesta_1_4)
+                }
+                2 ->{
+                    pregunta.text = getString(R.string.pregunta_2)
+                    option1.text = getString(R.string.respuesta_2_1)
+                    option2.text = getString(R.string.respuesta_2_2)
+                    option3.text = getString(R.string.respuesta_2_3)
+                    option4.text = getString(R.string.respuesta_2_4)
+                }
+                3 ->{
+                    pregunta.text = getString(R.string.pregunta_3)
+                    option1.text = getString(R.string.respuesta_3_1)
+                    option2.text = getString(R.string.respuesta_3_2)
+                    option3.text = getString(R.string.respuesta_3_3)
+                    option4.text = getString(R.string.respuesta_3_4)
+                }
+            }
+        }
+        preguntaActual()
         option1.setOnClickListener(){
             var respuesta = validarRespuesta(option1.text.toString())
             chamo.putBoolean("rappy",respuesta)
@@ -51,6 +81,10 @@ class QuestionFragment : Fragment(R.layout.fragment_question) {
         else{
             Toast.makeText(context, "Más Falso que Carlitos", Toast.LENGTH_LONG).show()
         }
+        if(validacion == true){
+            puntuacion++
+        }
         return validacion
     }
+
 }
